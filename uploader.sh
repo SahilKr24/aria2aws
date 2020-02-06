edcd0c67702e3406384bb50aa046ebb0fd1f0dbf
SahilKr24 <io.sahilkr@gmail.com>
2020-02-06 22:41:28 +0530

    Inital upload

--- uploader.sh
+++ uploader.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-ArialocalFolder="${HOME}/torrent"
+ArialocalFolder="${HOME}/downloads"
 AriauncompleteExt='.aria2'
 
 function UploadToGoogle() {
-	count=`ls /home/sahil/torrent/*.aria2 2>/dev/null | wc -l`
+	count=`ls ${HOME}/downloads/*.aria2 2>/dev/null | wc -l`
 	if [ $count != 0 ]
 	then
 		echo 'Download in progress'
 	else
-        	gdrive sync upload ${HOME}/torrent 1ssniNl0SunY3xoFVS_mkr_K5_EvU4pSu
-	        rm -rf ${HOME}/torrent/*
+        	/usr/bin/rclone move ${HOME}/downloads googledrive:
 	fi
 }
 #UploadToGoogle $BDlocalFolder $BDuncompleteExt
